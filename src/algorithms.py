7d083a6863013639bf06b57a66564d3d99a12f9b
migueltorrescosta <miguelptcosta1995@gmail.com>
2024-04-02 02:57:54 +0200

    Update MetropolisHastings algorithm

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,20 +1,19 @@
-from typing import TypeVar, Generic, List
-from tqdm import trange
+import datetime
 from abc import ABC, abstractmethod
+from typing import Generic, List, TypeVar
+
+import numpy as np
 from matplotlib import pyplot as plt
-from IPython.core.pylabtools import figsize
+from tqdm import trange
 
 T = TypeVar("T")
 
 
-figsize(20, 5)
-
-
 class AbstractMetropolisHastings(ABC, Generic[T]):
+    # Based on https://en.wikipedia.org/wiki/Metropolis%E2%80%93Hastings_algorithm#Description
+
     def __init__(self, initial_configuration: T):
-        self.configuration_history: List[T] = [
-            initial_configuration
-        ]  # This array evolves over time :D
+        self.configuration_history: List[T] = [initial_configuration]
         self.accepted_configuration_count: int = 0
         self.rejected_configuration_count: int = 0
 
@@ -27,8 +26,15 @@ class AbstractMetropolisHastings(ABC, Generic[T]):
         pass
 
     @abstractmethod
-    def approval_function(self, new_configuration: T) -> bool:
+    def state_likelihood(self, T) -> float:
+        # This is proportional to the state probability
         pass
+
+    def approval_function(self, new_configuration: T) -> bool:
+        return (
+            self.state_likelihood(new_configuration)
+            >= self.state_likelihood(self.current_configuration) * np.random.random()
+        )
 
     def run_single_iteration(self, limit_tries=10**5) -> T:
         tries = 0
@@ -51,11 +57,22 @@ class AbstractMetropolisHastings(ABC, Generic[T]):
         pbar = trange(n, desc="Bar desc", leave=True)
         for _ in pbar:
             self.run_single_iteration()
-            if (
-                self.accepted_configuration_count + self.rejected_configuration_count
-            ) % 10**3 == 0:
+
+            # Update the progress bar roughly once a second
+            seconds_passed = datetime.datetime.now().timestamp() - pbar.start_t
+            n = self.rejected_configuration_count + self.accepted_configuration_count
+            iterations_per_second = 1 + int(n / seconds_passed)
+            update_frequency = 2 ** (int(np.log(iterations_per_second) / np.log(2)) - 1)
+
+            if n % update_frequency == 0:
+
+                rejection_rate = np.divide(
+                    self.rejected_configuration_count,
+                    self.accepted_configuration_count
+                    + self.rejected_configuration_count,
+                )
                 pbar.set_description(
-                    f"({self.rejected_configuration_count}/{self.accepted_configuration_count  +self.rejected_configuration_count}) {self.current_configuration:e} ",
+                    f"Rejected {100*rejection_rate:.1f}%",
                     refresh=True,
                 )
 
