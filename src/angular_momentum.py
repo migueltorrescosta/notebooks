fd985337e457688a2e208500e22dd7b592e77e97
Miguel Pereira Torres Costa <miguelptcosta1995@gmail.com>
2026-04-22 18:40:58 +0200

    🚀 Vectorize spin matrix generation

--- src/angular_momentum.py
+++ src/angular_momentum.py
@@ -3,13 +3,17 @@ import numpy as np
 
 def generate_spin_matrices(dim: int) -> tuple[np.ndarray, np.ndarray]:
     spin = (dim - 1) / 2
+    # Vectorized construction of Jz (diagonal matrix)
+    j = np.arange(dim)
+    magnetic_numbers = spin - j
+    jz = np.diag(magnetic_numbers)
+
+    # Vectorized construction of Jx (off-diagonal matrix)
+    off_diags = 0.5 * np.sqrt(
+        (spin - magnetic_numbers[:-1] + 1) * (spin + magnetic_numbers[:-1])
+    )
     jx = np.zeros((dim, dim))
-    jz = np.zeros((dim, dim))
-    for j in range(dim):
-        magnetic_number = spin - j
-        jz[j, j] = magnetic_number
-        if j < dim - 1:
-            jx[j, j + 1] = jx[j + 1, j] = 0.5 * np.sqrt(
-                (spin - magnetic_number + 1) * (spin + magnetic_number)
-            )
+    jx[np.arange(dim - 1), np.arange(1, dim)] = off_diags
+    jx[np.arange(1, dim), np.arange(dim - 1)] = off_diags
+
     return np.array(jx, dtype=float), np.array(jz, dtype=float)
